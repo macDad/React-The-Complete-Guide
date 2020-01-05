4b266f8fd83a687eac10ecee338f489e6a97bf25
MadukaJ <jaya.maduka@gmail.com>
2020-01-05 21:47:57 +0800

    32. Creating a Functional Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
     //This code will compile to
     return (
         <div className="App">
             <h1>Hi Im React App</h1>
+            <p>This Also working</p>
+            <Person/>
         </div>
     );
     //this code -----> this is JSX
-    
-    // return React.createElement('div', {className: 'App'},
-    //                                 React.createElement('h1', null, 'Hi I\'m React App!!!'));
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React App!!!'));
 }
 
 export default App;
