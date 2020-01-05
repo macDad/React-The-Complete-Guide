decf1d2d0ab366561f15032afea364761d2ac90a
MadukaJ <jaya.maduka@gmail.com>
2020-01-05 22:33:09 +0800

    39. Props & State

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-    //This code will compile to
-    return (
-        <div className="App">
-            <h1>Hi Im React App</h1>
-            <p>This Also working</p>
-            <Person name="Mac" age="32"/>
-            <p>This Also working</p>
-            <Person name="Paro" age="28">My Hobbies: Racing</Person>
-            <p>This Also working</p>
-            <Person name="Kyan" age="1"/>
-        </div>
-    );
-    //this code -----> this is JSX
+class App extends Component {
+    state = {
+        persons: [
+            {name: "Mac", age: 32},
+            {name: "Paro", age: 28},
+            {name: "Kyan", age: 1}
+        ]
+    }
 
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React App!!!'));
+    render() {
+        //This code will compile to
+        return (
+            <div className="App">
+                <h1>Hi Im React App</h1>
+                <p>This Also working</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <p>This Also working</p>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+                <p>This Also working</p>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        //this code -----> this is JSX
+
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React App!!!'));
+    }
 }
 
 export default App;
