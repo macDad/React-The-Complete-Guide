16ad6bc7c43d2faa27120f0fda96aa06f28b1876
MadukaJ <jaya.maduka@gmail.com>
2020-01-06 22:07:48 +0800

    44. Using the useState() Hook for State Manipulation

--- Readme.md
+++ Readme.md
@@ -191,3 +191,30 @@ npx create-react-app react-complete-guide --script-version 1.1.5
 
 ####  Handling Events with Methods
 https://reactjs.org/docs/events.html#supported-events
+
+
+#### Function Components Naming
+
+In the **next lecture**, we'll learn how to manage state in functional components (instead of class-based components).
+
+In case you're getting an error with the code shown in the next lecture, simply assign a capitalized variable name to the variable that holds your functional component.
+
+###### Example:
+
+Use
+
+```
+const App = () => { ... }
+```
+instead of
+
+```
+const app = () => { ... }
+```
+Technically, that's not required but depending on your project setup, the built-in linter (a code quality checking tool) that comes with create-react-app might not like the lowercase variable name.
+
+You can also avoid this by creating projects with the **right react-scripts version**, which I would **recommend for this course** anyways (see the setup video, lecture 26)
+
+```
+> create-react-app my-app --scripts-version 1.1.5
+```
--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,45 +1,42 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-    state = {
+const App = props => {
+
+    const [personsState, setPersonsState] = useState({
         persons: [
             {name: "Mac", age: 32},
             {name: "Paro", age: 28},
             {name: "Kyan", age: 1}
-        ],
-        otherState: 'some other values'
-    }
-    switchNameHandler = () => {
-        // console.log('was clicked!');
-        // DON'T DO THIS: this.state.persons[0].name = "Maduka J";
-        this.setState({
+        ]
+    });
+    const [otherState, setOtherState] = useState('some other values');
+
+    console.log(personsState, otherState);
+
+    const switchNameHandler = () => {
+        setPersonsState({
             persons: [
                 {name: "Mac J", age: 32},
                 {name: "Paro", age: 28},
                 {name: "Kyan", age: 1}
             ]
         })
-    }
+    };
 
-
-    render() {
-        //This code will compile to
-        return (
-            <div className="App">
-                <h1>Hi Im React App</h1>
-                <p>This Also working</p>
-                <button onClick={this.switchNameHandler}>Switch Name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            </div>
-        );
-        //this code -----> this is JSX
-
-        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React App!!!'));
-    }
-}
+    return (
+        <div className="App">
+            <h1>Hi Im React App</h1>
+            <p>This Also working</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        </div>
+    );
+};
 
 export default App;
+
+
