11565f7beb097786bdad92599864acdf73c128b9
MadukaJ <jaya.maduka@gmail.com>
2020-01-08 23:07:38 +0800

    53. Rendering Content Conditionally

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,6 +12,7 @@ const App = props => {
         ]
     });
     const [otherState, setOtherState] = useState('some other values');
+    const [showPersons, setShowPersons] = useState(false);
 
     const switchNameHandler = (newName) => {
         setPersonsState({
@@ -31,6 +32,10 @@ const App = props => {
             ]
         })
     };
+    const togglePersonHandler = () => {
+        const doesShow = showPersons;
+        setShowPersons(!doesShow);
+    };
     const style = {
         backgroundColor: 'white',
         font: 'inherit',
@@ -43,15 +48,18 @@ const App = props => {
             <h1>Hi Im React App</h1>
             <p>This Also working</p>
             <button style={style}
-                    onClick={() => {
-                        switchNameHandler('Maduka A J !!')
-                    }}>Switch Name
+                    onClick={togglePersonHandler}>Switch Name
             </button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}
-                    click={switchNameHandler.bind(this, 'Maduka A J R M')}
-                    changed={nameChangeHandler}>My Hobbies: Racing</Person>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+            {
+                showPersons === true ?
+                    <div>
+                        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+                        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}
+                                click={switchNameHandler.bind(this, 'Maduka A J R M')}
+                                changed={nameChangeHandler}>My Hobbies: Racing</Person>
+                        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+                    </div> : null
+            }
         </div>
     );
 };
