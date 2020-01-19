cc8790ba71f0f0f0f918bec4e77ebc09027e754c
MadukaJ <jaya.maduka@gmail.com>
2020-01-19 20:01:01 +0800

    93. Using useEffect() in Functional Components

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import classes from './Cockpit.css';
 
-const cockpit = ( props ) => {
+const cockpit = (props) => {
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+        //HTTP request
+    });
     const assignedClasses = [];
     let btnClass = '';
     if (props.showPersons) {
         btnClass = classes.Red;
     }
 
-    if ( props.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red); // classes = ['red']
     }
-    if ( props.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
-            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button
                 className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+                onClick={props.clicked}>Toggle Persons
+            </button>
         </div>
     );
 };
