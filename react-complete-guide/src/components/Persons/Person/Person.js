8d3b5903c0dd3351098b76062715d478b6fc2448
MadukaJ <jaya.maduka@gmail.com>
2020-01-25 11:58:29 +0800

    105. Higher Order Components (HOC) - Introduction

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,11 +1,11 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Auxiliary from "../../../hoc/Auxiliary";
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Fragment>
+            <Auxiliary>
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
@@ -15,7 +15,7 @@ class Person extends Component {
                     onChange={this.props.changed}
                     value={this.props.name}
                 />
-            </Fragment>
+            </Auxiliary>
         );
     }
 }
