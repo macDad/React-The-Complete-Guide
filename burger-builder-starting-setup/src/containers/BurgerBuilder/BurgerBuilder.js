11de4c5a401434c6c0bb4d167c1ff115c504072a
MadukaJ <jaya.maduka@gmail.com>
2020-01-27 11:51:53 +0800

    156. Adding a Dynamic Ingredient Component 157. Adding Prop Type Validation 158. Starting the Burger Component

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
 import Auxiliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
     render() {
         return (
             <Auxiliary>
-                <div>Burger</div>
-                <div>Burger Controls</div>
+                <Burger/>
+                <div>Bulid Controls</div>
             </Auxiliary>
         );
     }
