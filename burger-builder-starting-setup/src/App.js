025673594634a183c50ff826129c8f9375d1a789
MadukaJ <jaya.maduka@gmail.com>
2020-01-27 08:21:57 +0800

    154. Creating a Layout Component

--- burger-builder-starting-setup/src/App.js
+++ burger-builder-starting-setup/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <div>
-
-
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <p>Tset</p>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
